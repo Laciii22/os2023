413b34868e91f0c7d4ade32cc8f0d700e39ec3ad
Ladislav Gardian <gardian.laci@gmail.com>
2023-10-08 12:56:37 +0200

    page table vypis

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,30 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+//vmprintf starting
+void
+printpgtb(pagetable_t pagetable, int depth)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      printf("..");
+      for (int j=0; j<depth; j++){
+        printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      }
+     if ((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X))==0){
+       uint64 child = PTE2PA(pte);
+       printpgtb((pagetable_t)child, depth+1);
+        }
+      }
+    }
+  }
+
+void
+vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  printpgtb(pagetable, 0);
+}
+
