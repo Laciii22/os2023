af6849e9ec4efe0692d5b437ecfed07a754b8bb4
Ladislav Gardian <gardian.laci@gmail.com>
2023-12-05 11:41:55 +0100

    net lab

--- kernel/pci.c
+++ kernel/pci.c
@@ -1,8 +1,3 @@
-//
-// simple PCI-Express initialization, only
-// works for qemu and its e1000 card.
-//
-
 #include "types.h"
 #include "param.h"
 #include "memlayout.h"
@@ -11,51 +6,44 @@
 #include "proc.h"
 #include "defs.h"
 
-void
-pci_init()
-{
-  // we'll place the e1000 registers at this address.
-  // vm.c maps this range.
-  uint64 e1000_regs = 0x40000000L;
+void pci_init() {
+    // Remove the unused typedef
+    // typedef uint64 pagetable_t;
 
-  // qemu -machine virt puts PCIe config space here.
-  // vm.c maps this range.
-  uint32  *ecam = (uint32 *) 0x30000000L;
-  
-  // look at each possible PCI device on bus 0.
-  for(int dev = 0; dev < 32; dev++){
-    int bus = 0;
-    int func = 0;
-    int offset = 0;
-    uint32 off = (bus << 16) | (dev << 11) | (func << 8) | (offset);
-    volatile uint32 *base = ecam + off;
-    uint32 id = base[0];
-    
-    // 100e:8086 is an e1000
-    if(id == 0x100e8086){
-      // command and status register.
-      // bit 0 : I/O access enable
-      // bit 1 : memory access enable
-      // bit 2 : enable mastering
-      base[1] = 7;
-      __sync_synchronize();
+    // Replace magic constants with meaningful names
+    const uint64 e1000_regs = 0x40000000L;
+    uint32 *ecam = (uint32 *)0x30000000L;
 
-      for(int i = 0; i < 6; i++){
-        uint32 old = base[4+i];
+    // Configure PCI devices
+    for (int dev = 0; dev < 32; dev++) {
+        int bus = 0;
+        int func = 0;
+        int offset = 0;
+        uint32 off = (bus << 16) | (dev << 11) | (func << 8) | (offset);
+        volatile uint32 *base = ecam + off;
+        uint32 id = base[0];
 
-        // writing all 1's to the BAR causes it to be
-        // replaced with its size.
-        base[4+i] = 0xffffffff;
-        __sync_synchronize();
+        // Check if the device is an e1000
+        if (id == 0x100e8086) {
+            // Command and status register.
+            // Bit 0: I/O access enable, Bit 1: Memory access enable, Bit 2: Enable mastering
+            base[1] = 7;
+            __sync_synchronize();
 
-        base[4+i] = old;
-      }
+            // Reset the BARs to get their sizes
+            for (int i = 0; i < 6; i++) {
+                uint32 old = base[4 + i];
+                base[4 + i] = 0xffffffff;
+                __sync_synchronize();
+                base[4 + i] = old;
+            }
 
-      // tell the e1000 to reveal its registers at
-      // physical address 0x40000000.
-      base[4+0] = e1000_regs;
+            // Tell the e1000 to reveal its registers at physical address 0x40000000
+            base[4 + 0] = e1000_regs;
 
-      e1000_init((uint32*)e1000_regs);
+            // Initialize the e1000
+            e1000_init((uint32 *)e1000_regs);
+        }
     }
-  }
 }
+
